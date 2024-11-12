82d35060ef5e0eac4395f6ef2792457927b43bfc
J24Chen <Jchen2408@gmail.com>
2024-11-12 14:09:12 -0500

    add all basic node modules

---
+++ src/home.js
@@ -0,0 +1,7 @@
+
+const content = document.querySelector(".content");
+
+function createContent() {
+    let h1 = document.createElement("h1");
+    
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+console.log("よろしく");
+
+
+import "./style.css";
+
+
+
+
+/* 
+TODO:
+
+- Figure out how to import fonts through config.js (Cinzel)
+- Create the header with buttons
+- Make the home content in home.js and append it, 
+- Create the other sections and configure tab switching
+
+
+*/
